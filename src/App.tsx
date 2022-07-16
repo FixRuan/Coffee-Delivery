b7e661dca92ab39bb27ea1a1f4fe92c3f92bb0d3
Ruan Pablo <ruan_goio10@hotmail.com>
2022-07-16 15:05:17 -0300

    Layout Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -7,7 +9,9 @@ export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
 			<GlobalStyle />
-			<div>Hello World</div>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
 		</ThemeProvider>
 	);
 }
---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { DefaultLayout } from "./Layout/DefaultLayout";
+import { HomePage } from "./pages/Home";
+import { OrderPage } from "./pages/Order";
+
+export function Router() {
+	return (
+		<Routes>
+			<Route path="/" element={<DefaultLayout />}>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/order" element={<OrderPage />} />
+			</Route>
+		</Routes>
+	);
+}
