eb6433ef62466351f5e90474bf6502a5b7516e5e
Ruan Pablo <ruan_goio10@hotmail.com>
2022-07-16 14:46:30 -0300

    Global Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
-
-function App() {
-  return (
-    <div>
-
-    </div>
-  )
+export function App() {
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<div>Hello World</div>
+		</ThemeProvider>
+	);
 }
-
-export default App
